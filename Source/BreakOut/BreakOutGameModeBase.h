05160d1039a56e0379b56ddabfe15513eac1d5d2
atmterminated <69296547+atmterminated@users.noreply.github.com>
2020-08-06 20:20:38 +1200

    UPDATE GAME

---
+++ Source/BreakOut/BreakOutGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BreakOutGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BREAKOUT_API ABreakOutGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
